c699b70fe1eca473fda7fa1996cdcea7ea6f6bcb
salvatore@rezo.net <>
2012-01-05 08:09:29 +0000

    langues (paquet-themes)

--- lang/paquet-themes_fr.php
+++ lang/paquet-themes_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-themes
-// Langue: fr
-// Date: 30-07-2011 15:01:38
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/themes/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// T
+	// T
 	'themes_description' => 'Des thèmes pour l\'espace privé',
-	'themes_slogan' => 'Des thèmes pour l\'espace privé',
+	'themes_slogan' => 'Des thèmes pour l\'espace privé'
 );
+
 ?>
