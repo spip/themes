c02d8245a6df07dbe01ffd9503e078b9c53090ed
serge.markitanenko@gmail.com <>
2012-04-03 22:29:21 +0000

    langues (paquet-themes)

---
+++ lang/paquet-themes_ru.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-themes?lang_cible=ru
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'themes_description' => 'Темы оформления административной части',
+	'themes_slogan' => 'Темы оформления административной части'
+);
+
+?>
