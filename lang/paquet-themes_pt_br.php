a5cc5cd919028339b3265e0bffb6c1ab80aba790
rporto@calepino.com <>
2016-09-26 03:33:10 +0000

    [Salvatore] [source:_core_/plugins/themes/lang/ paquet-themes] Export depuis http://trad.spip.net de la langue pt_br

---
+++ lang/paquet-themes_pt_br.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-themes?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'themes_description' => 'Temas para a área restrita',
+	'themes_slogan' => 'Temas para a área restrita'
+);
